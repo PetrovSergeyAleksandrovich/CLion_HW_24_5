9f94dd554af7c908d081a24c6dd6e79cee4a789f
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-03-28 14:58:35 +0300

    init cmt

---
+++ cmake-build-debug/CMakeFiles/CLion_HW_24_5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CLion_HW_24_5.exe"
+  "CLion_HW_24_5.exe.manifest"
+  "CLion_HW_24_5.lib"
+  "CLion_HW_24_5.pdb"
+  "CMakeFiles/CLion_HW_24_5.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLion_HW_24_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CLion_HW_24_5.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CLion_HW_24_5.
+# This may be replaced when dependencies are built.

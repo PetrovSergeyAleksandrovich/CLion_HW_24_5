49d9733a8b1414ead00630862dfe549c5037dc71
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-03-30 18:43:42 +0300

    task 3 updated with functions

--- HW_24_5_task_3.cpp
+++ HW_24_5_task_3.cpp
@@ -1,29 +1,45 @@
 #include <iostream>
 #include <vector>
 
-struct x_coordinates
-{
-    std::vector<double> x = {};
-};
-
-struct y_coordinates
-{
-    std::vector<double> y = {};
-};
-
 struct vec
 {
-    x_coordinates x = {};
-    y_coordinates y = {};
+    std::vector<double> x = {};
+    std::vector<double> y = {};
     int scale = 0;
-    double length = 0;
 };
 
+void add(double& x1, double& x2, double& y1, double& y2)
+{
+    std::cout << "result of 'add' = " << x1 + x2 << " , " << y1 + y2 << " ) ";
+}
+
+void subtract(double& x1, double& x2, double& y1, double& y2)
+{
+    std::cout << "result of 'subtract' = " << x1 - x2 << " , " << y1 - y2 << " ) ";
+}
+
+void scale(double& x1, double& y1, int& scale)
+{
+    std::cout << "result of 'scale' = " << x1 * scale << " , " << y1 * scale << " ) ";
+}
+
+void length(double& x1, double& y1)
+{
+    std::cout << "result of 'length' = " << sqrt( ((x1 * x1) + (y1 * y1)) );;
+}
+
+void normalize(double& x1, double& y1)
+{
+    std::cout
+    << "result of 'normalize' = "
+    << x1 / sqrt( ((x1 * x1) + (y1 * y1)) )
+    << " , "
+    << y1 / sqrt( ((x1 * x1) + (y1 * y1)) );
+}
 
 int main()
 {
-
-    vec a = { {{}}, {{}}, 0, 0 };
+    vec a = {};
     double tmp;
 
     std::string user_input;
@@ -32,80 +48,76 @@ int main()
 
     if(user_input == "add")
     {
-        std::cout << "input first vector coordinate X: ";
+        std::cout << "input first vector coordinate X : ";
         std::cin >> tmp;
-        a.x.x.push_back(tmp);
-        std::cout << "input first vector coordinate Y: ";
+        a.x.push_back(tmp);
+        std::cout << "input first vector coordinate Y : ";
         std::cin >> tmp;
-        a.y.y.push_back(tmp);
-        std::cout << "input second vector coordinate X: ";
+        a.y.push_back(tmp);
+        std::cout << "input second vector coordinate X : ";
         std::cin >> tmp;
-        a.x.x.push_back(tmp);
-        std::cout << "input second vector coordinate Y: ";
+        a.x.push_back(tmp);
+        std::cout << "input second vector coordinate Y : ";
         std::cin >> tmp;
-        a.y.y.push_back(tmp);
+        a.y.push_back(tmp);
 
-        std::cout << "result of 'add' = ( " << a.x.x[0] + a.x.x[1] << " , " << a.y.y[0] + a.y.y[1] << " ) ";
+        add(a.x[0], a.x[1], a.y[0], a.y[1] );
     }
 
     if(user_input == "subtract")
     {
-        std::cout << "input first vector coordinate X: ";
+        std::cout << "input first vector coordinate X : ";
         std::cin >> tmp;
-        a.x.x.push_back(tmp);
-        std::cout << "input first vector coordinate Y: ";
+        a.x.push_back(tmp);
+        std::cout << "input first vector coordinate Y : ";
         std::cin >> tmp;
-        a.y.y.push_back(tmp);
-        std::cout << "input second vector coordinate X: ";
+        a.y.push_back(tmp);
+        std::cout << "input second vector coordinate X : ";
         std::cin >> tmp;
-        a.x.x.push_back(tmp);
-        std::cout << "input second vector coordinate Y: ";
+        a.x.push_back(tmp);
+        std::cout << "input second vector coordinate Y : ";
         std::cin >> tmp;
-        a.y.y.push_back(tmp);
+        a.y.push_back(tmp);
 
-        std::cout << "result of 'subtract' = ( " << a.x.x[0] - a.x.x[1] << " , " << a.y.y[0] - a.y.y[1] << " ) ";
+        subtract(a.x[0], a.x[1], a.y[0], a.y[1] );
     }
 
     if(user_input == "scale")
     {
         std::cout << "input first vector coordinate X: ";
         std::cin >> tmp;
-        a.x.x.push_back(tmp);
+        a.x.push_back(tmp);
         std::cout << "input first vector coordinate Y: ";
         std::cin >> tmp;
-        a.y.y.push_back(tmp);
+        a.y.push_back(tmp);
         std::cout << "input scale value: ";
         std::cin >> a.scale;
 
-        std::cout << "result of 'scale' = ( " << a.x.x[0] * a.scale << " , " << a.y.y[0] * a.scale << " ) ";
+        scale(a.x[0], a.y[0], a.scale);
     }
 
     if(user_input == "length")
     {
         std::cout << "input first vector coordinate X: ";
         std::cin >> tmp;
-        a.x.x.push_back(tmp);
+        a.x.push_back(tmp);
         std::cout << "input first vector coordinate Y: ";
         std::cin >> tmp;
-        a.y.y.push_back(tmp);
+        a.y.push_back(tmp);
 
-        a.length = sqrt( (a.x.x[0] * a.x.x[0]) + (a.y.y[0] * a.y.y[0]) );
-
-        std::cout << "result of 'length' = " << a.length;
+        length(a.x[0], a.y[0]);
     }
 
     if(user_input == "normalize")
     {
         std::cout << "input first vector coordinate X: ";
         std::cin >> tmp;
-        a.x.x.push_back(tmp);
+        a.x.push_back(tmp);
         std::cout << "input first vector coordinate Y: ";
         std::cin >> tmp;
-        a.y.y.push_back(tmp);
+        a.y.push_back(tmp);
 
-        a.length = sqrt( (a.x.x[0] * a.x.x[0]) + (a.y.y[0] * a.y.y[0]) );
-
-        std::cout << "result of 'normalize' = " << a.x.x[0] / a.length << " , " << a.y.y[0] / a.length;
+        normalize(a.x[0], a.y[0]);
     }
 
     return 0;
